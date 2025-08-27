4163e85369b90ebb04bcc5830c2a95e6cc1088f7
diogofoll <diogo.follmann.ifpr.2023@gmail.com>
2025-08-27 15:16:01 -0300

    multipla escolha
    multipla escolha js

---
+++ multipla escolha/js/ex00.js
@@ -0,0 +1,34 @@
+var dia
+
+dia = parseInt(prompt("digite o numero do dia da semana correspondente: "))
+
+switch(dia){
+    case 1:{
+        alert("Domingo")
+        break
+    }
+    case 2:{
+        alert("Segunda")
+        break
+    }
+    case 3:{
+        alert("Terça")
+        break
+    }
+    caso 4:{
+        alert("Quarta")
+        break
+    }
+    caso 5:{
+        alert("Quinta")
+        break
+    }
+    caso 6:{
+        alert("Sexta")
+        break
+    }
+    caso 7:{
+        alert("Sabado")
+        break
+    }
+}
