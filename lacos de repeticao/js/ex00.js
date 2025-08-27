67a103c6b44a1fbc52a226e09870c096012e7f4f
diogofoll <diogo.follmann.ifpr.2023@gmail.com>
2025-08-27 15:52:06 -0300

    repetiçao
    laços de repeticao

---
+++ lacos de repeticao/js/ex00.js
@@ -0,0 +1,22 @@
+var cont 
+
+cont = 0
+while(cnt<3){
+    alert("aborrecendo o usuario"+cont+ "x")
+    cont++
+}
+
+for(cont=0;cont<5;cont++)
+{
+    if(cont==0)
+        {
+    alert("clique uma vez")
+        }
+    else{
+        alert("continue clicando!")
+    }
+}
+    var resposta
+    do{
+        resposta=confirm("deseja continuar?")
+    }while(resposta==true)
