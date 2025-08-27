c8b746104c6da187d7b10401c80147f9d7541f10
diogofoll <diogo.follmann.ifpr.2023@gmail.com>
2025-08-27 15:05:04 -0300

    implementacao inicial do js
    implementacao inicial do js

--- Estruturas_Condicionais/js/ex00.js
+++ Estruturas_Condicionais/js/ex00.js
@@ -1,1 +1,11 @@
-l
+var nome, idade
+
+nome=prompt("Digite seu nome")
+idade=parseInt(prompt("digite a sua idade"))
+
+if (idade >= 18){
+    alert(nome +"você já pode tentar tirar a cnh")
+}
+else{
+    alert(nome +"você ainda não tem idade para tirar a cnh")
+}
